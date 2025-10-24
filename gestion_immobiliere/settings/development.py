6cf64c2114c3f78bc4ade60fa3ab8eae520c4d7c
camille1407 <edohcamille7@gmail.com>
2025-10-24 14:52:12 +0000

    domainename

--- gestion_immobiliere/settings/development.py
+++ gestion_immobiliere/settings/development.py
@@ -6,7 +6,9 @@ from .base import * # Importe tous les paramètres communs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gestionimmobiliere-production-3fff.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://gestionimmobiliere-production-3fff.up.railway.app')
 
 # Base de données pour le développement
 DATABASES = {
