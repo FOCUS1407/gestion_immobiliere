cf734c430895c1811fedc1c9d47714a998864321
camille1407 <edohcamille7@gmail.com>
2025-11-14 19:41:29 +0000

    production

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,9 @@ echo "Static files collected successfully."
 echo "Starting Gunicorn server with Gunicorn..."
 # Utilisation de --log-level info pour des logs plus concis en production
 # Les autres options sont conservées pour la performance et la robustesse
-exec gunicorn --chdir /app gestion_immobiliere.wsgi:application --bind 0.0.0.0:$PORT --log-level info --timeout 120 --workers 2
+# --workers 3 : Nombre de processus pour gérer les requêtes.
+# --timeout 120 : Augmente le délai d'attente à 120 secondes pour donner à l'application le temps de démarrer.
+exec gunicorn gestion_immobiliere.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
+
 
 # --- Fin du script d'entrée (ne devrait pas être atteint si Gunicorn démarre) ---
